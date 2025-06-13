0f85faa784f1b4b1172b322ac1da187fcdeb0a07
Vani-31 <vaaniiiagrawal1234@gmail.com>
2025-06-13 12:59:39 +0530

    if series program using in and not in

---
+++ if/favfruit.py
@@ -0,0 +1,11 @@
+fav_fruit=["apple","banana","mango"]
+if "apple"in fav_fruit:
+    print("ohh i dont like apple that much")
+if "mango" in fav_fruit:
+    print("yaay season of mango is this")
+if "banana" in fav_fruit:
+    print("i love bananas")
+if "jamun" not in fav_fruit:
+    print("you forgot jamun :[")
+if "apple" not in fav_fruit:
+    print("i could eat them if necessary")

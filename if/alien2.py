c0cb9cad40322cfaefb7d2def68299eec0033f02
Vani-31 <vaaniiiagrawal1234@gmail.com>
2025-06-13 12:45:27 +0530

    changing it to if - elif- else

--- if/alien2.py
+++ if/alien2.py
@@ -1,5 +1,8 @@
 alien_color=input("enter the color")
 if(alien_color.lower()=="green"):
-    print("Hey , you earned 5 points for shooting the alien...")
-else:
+    print("Hey , you earned 5 points .")
+elif(alien_color.lower()=="yellow"):
     print("YAY! you earned 10 points .")
+elif(alien_color.lower()=="red"):
+    print("YAY! you earned 15 points .")
+

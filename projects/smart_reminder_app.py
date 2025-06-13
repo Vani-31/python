1bff3b7f04d92c80b522419df4b48e3652cb35a2
Vani-31 <vaaniiiagrawal1234@gmail.com>
2025-06-13 13:36:48 +0530

    it is a smart reminder app

---
+++ projects/smart_reminder_app.py
@@ -0,0 +1,14 @@
+mood=input("HEY! howz your mood today :")
+
+if mood.lower()=="happy":
+    print("heyy i am very happy that you are have have a great day ahead😊.")
+elif mood.lower()=="tired":
+    print("hey  take some rest you worked hard . you deserve some rest 😎.")
+elif mood.lower()=="stressed":
+    print("hey listen  and take a deep breath and stressing wont change anything . take a chill pill every thing will be okk you should not take stress on thingsyou cant change 👍.")
+elif mood.lower()=="bored":
+    print("hey , you can do some thing you enjoy maybe watch some pod cast , do some craft , or listen to music or recent newz , coding wont hurt btw ...😁😁")
+elif mood.lower()=="excited":
+    print("woww i am also excited for you . you should be like this .😉")
+else :
+    print("Mood not recognized .Want to try again?🤔🤔")

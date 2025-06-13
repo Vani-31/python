d4ea15443e2e35e68a810a924d6043eb19e38d0a
Vani-31 <vaaniiiagrawal1234@gmail.com>
2025-06-14 00:06:45 +0530

    age checker app

---
+++ projects/age_checker_app.py
@@ -0,0 +1,16 @@
+#age checking app
+age =int(input(" what is your age ?😁"))
+if age>=1 and age<5:
+    print( "You're just a cute little baby! 👶")
+elif age>=5 and age<=12:
+    print("Hey kiddo! Life's an adventure 🛝")
+elif age>=13 and age<=19:
+    print("Teen mode: activated. Study + drama 😎")
+elif age>=20 and age<=29:
+    print("Twenties! Time to build, learn, and grow 🌱")
+elif age>=30 and age<=59:
+    print("Adulting in progress... Stay calm 🧘")
+elif age>=60:
+    print("A lifetime of wisdom — you're golden 🌟")
+else:
+    print("Hmm... that doesn't seem like a real age! 🤔")

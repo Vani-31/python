fa9c6298e6fc6b25cb252c44cf01468fb6dac545
Vani-31 <vaaniiiagrawal1234@gmail.com>
2025-06-13 12:38:48 +0530

    some if else exercises

---
+++ if/alien1.py
@@ -0,0 +1,7 @@
+alien_color = "yellow"
+if(alien_color=="green"):
+    print("YAYY! You earned 5 points .")
+
+alien_color ="green"
+if(alien_color=="green"):
+    print("YAYY! You earned 5 points .")
---
+++ if/alien2.py
@@ -0,0 +1,5 @@
+alien_color=input("enter the color")
+if(alien_color.lower()=="green"):
+    print("Hey , you earned 5 points for shooting the alien...")
+else:
+    print("YAY! you earned 10 points .")
